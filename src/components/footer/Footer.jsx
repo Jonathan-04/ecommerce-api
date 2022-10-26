5343c4651d4def156897fbe5b010deb9e3d8d044
Jonathan <jonathanvel84@gmail.com>
2022-10-26 13:57:22 -0500

    Realizado el Home

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Footer.css";
+import Logo from "../icons/Logo";
+import GooglePlay from "../../assets/img/googlestore.png";
+import IosStore from "../../assets/img/iosstore.png";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="container-footer">
+          <div className="text-footer">
+            <h1>
+              <Logo />
+              JesShop
+            </h1>
+            <ul>
+              <li>Quienes Somos</li>
+              <li>Política de Privacidad</li>
+              <li>Quiero Vender</li>
+              <li>PQRS</li>
+            </ul>
+          </div>
+          <div className="download-app">
+            <h2>Descarga la App</h2>
+            <img src={GooglePlay} alt="GooglePlay" />
+            <img src={IosStore} alt="IosStore" />
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Home.css";
+import Footer from "../../components/footer/Footer";
 import { AiOutlineShopping } from "react-icons/ai";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaTiktok } from "react-icons/fa";
+import { AiFillFacebook } from "react-icons/ai";
+
 import Banner from "../../assets/img/Banner.jpg";
-import Tenis from "../../assets/img/Tenis.jpg";
+import Tenis from "../../assets/img/tenis.jpg";
 import Diademas from "../../assets/img/Diademas.png";
+import Video from "../../assets/video/video.mp4";
+import MercadoPago from "../../assets/img/mercadopago.png";
+import Paypal from "../../assets/img/paypal.png";
+import Nequi from "../../assets/img/NEQUI.png";
 
 export default function Home() {
   return (
@@ -60,17 +69,94 @@ export default function Home() {
               <h2>Productos con la mejor calidad</h2>
               <NavLink>Comprar</NavLink>
             </div>
-            <img src={Diademas} alt="" />
+            <img id="img-buy" src={Diademas} alt="" />
           </article>
           <article className="item-buy">
             <div className="text-item-buy">
               <h2>Productos con la mejor calidad</h2>
               <NavLink>Comprar</NavLink>
             </div>
-            <img src={Diademas} alt="" />
+            <img id="img-buy" src={Diademas} alt="" />
           </article>
         </section>
+        <section className="section-video">
+          <div className="text-video">
+            <h1>Sigue tu camino, Persigue tus sueños!</h1>
+            <h2>#JesShop</h2>
+            <ul className="icons-social-media">
+              <li>
+                <a href="#">
+                  <AiFillInstagram size={28} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaTiktok size={24} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <AiFillFacebook size={28} />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <video src={Video} autoPlay muted loop></video>
+        </section>
+        <section className="section-interes">
+          <div className="title-section-product">
+            <h2>Te puede Interesar</h2>
+            <NavLink>
+              Ver mas <AiOutlineArrowRight />
+            </NavLink>
+          </div>
+          <article className="item-product">
+            <img src={Tenis} alt="Tenis" />
+            <h2>Producto 1</h2>
+            <div className="price">
+              <p>$120.000</p>
+              <AiOutlineShopping size={24} />
+            </div>
+          </article>
+          <article className="item-product">
+            <img src={Tenis} alt="Tenis" />
+            <h2>Producto 1</h2>
+            <div className="price">
+              <p>$120.000</p>
+              <AiOutlineShopping size={24} />
+            </div>
+          </article>
+          <article className="item-product">
+            <img src={Tenis} alt="Tenis" />
+            <h2>Producto 1</h2>
+            <div className="price">
+              <p>$120.000</p>
+              <AiOutlineShopping size={24} />
+            </div>
+          </article>
+          <article className="item-product">
+            <img src={Tenis} alt="Tenis" />
+            <h2>Producto 1</h2>
+            <div className="price">
+              <p>$120.000</p>
+              <AiOutlineShopping size={24} />
+            </div>
+          </article>
+        </section>
+        <section className="payment">
+          <h2>Pagos Seguros!</h2>
+          <div className="payment-img">
+            <img src={MercadoPago} alt="MercadoPago" />
+          </div>
+          <div className="payment-img">
+            <img src={Paypal} alt="Paypal" />
+          </div>
+          <div className="payment-img">
+            <img src={Nequi} alt="Nequi" />
+          </div>
+        </section>
       </main>
+      <Footer />
     </>
   );
 }
