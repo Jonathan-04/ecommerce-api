399fb7b333920d72e37b58e5e227b2000fb317be
Jonathan <jonathanvel84@gmail.com>
2022-11-08 21:57:25 -0500

    Validacion del Login y Registro

--- src/views/login/LoginAccount.jsx
+++ src/views/login/LoginAccount.jsx
@@ -1,23 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export default function LoginAccount() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    alert(`Has iniciado Sesión! ${data.email}`);
+  };
+
   return (
     <section className="container-login">
-      <form className="login">
+      <form className="login" onSubmit={handleSubmit(onSubmit)}>
         <h1>Login</h1>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Correo Electrónico"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Contraseña"
-        />
+        <div className="input-container">
+          <input
+            type="email"
+            name="email"
+            placeholder="Correo: ejemplo@gmail.com"
+            {...register("email", {
+              required: true,
+              pattern: /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/,
+            })}
+          />
+          {errors.email?.type === "required" && (
+            <span className="input-message-error">
+              El campo no puede estar vacío
+            </span>
+          )}
+          {errors.email?.type === "pattern" && (
+            <span className="input-message-error">El correo no es valido</span>
+          )}
+        </div>
+
+        <div className="input-container">
+          <input
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            {...register("password", {
+              required: true,
+            })}
+          />
+          {errors.password?.type === "required" && (
+            <span className="input-message-error">
+              El campo no puede estar vacío
+            </span>
+          )}
+        </div>
         <button>Iniciar</button>
         <ul className="login-options">
           <li>
--- src/views/login/Register.jsx
+++ src/views/login/Register.jsx
@@ -1,30 +1,90 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 export default function Register() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    alert(`Registro exitoso ${data.name} ${data.lastName}`);
+  };
   return (
     <section className="container-login">
-      <form className="login register">
+      <form className="login register" onSubmit={handleSubmit(onSubmit)}>
         <h1>Registro</h1>
-        <input type="text" className="name" id="name" placeholder="Nombres" />
-        <input
-          type="text"
-          className="lastname"
-          id="lastname"
-          placeholder="Apellidos"
-        />
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Correo Electrónico"
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Contraseña"
-        />
+        <div className="input-container inputName">
+          <input
+            type="text"
+            placeholder="Nombres"
+            {...register("name", {
+              required: true,
+            })}
+          />
+          {errors.name?.type === "required" && (
+            <span className="input-message-error">
+              El campo no puede estar vacío
+            </span>
+          )}
+        </div>
+        <div className="input-container inputLastName">
+          <input
+            type="text"
+            placeholder="Apellidos"
+            {...register("lastName", {
+              required: true,
+            })}
+          />
+          {errors.lastName?.type === "required" && (
+            <span className="input-message-error">
+              El campo no puede estar vacío
+            </span>
+          )}
+        </div>
+        <div className="input-container">
+          <input
+            type="email"
+            placeholder="Correo Electrónico"
+            {...register("email", {
+              required: true,
+              pattern: /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/,
+            })}
+          />
+          {errors.email?.type === "required" && (
+            <span className="input-message-error">
+              El campo no puede estar vacío
+            </span>
+          )}
+          {errors.email?.type === "pattern" && (
+            <span className="input-message-error">El correo no es valido</span>
+          )}
+        </div>
+        <div className="input-container">
+          <input
+            type="password"
+            placeholder="Contraseña"
+            {...register("password", {
+              required: true,
+              pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,20}$/,
+            })}
+          />
+          {errors.password?.type === "required" && (
+            <span className="input-message-error">
+              El campo no puede estar vacío
+            </span>
+          )}
+          {errors.password?.type === "pattern" && (
+            <span className="input-message-error">
+              La contraseña debe contener: 1 letra minúscula, 1 letra mayúscula,
+              al menos un número, sin espacios, mínimo 8 carácteres.
+            </span>
+          )}
+        </div>
         <button>Registrarme</button>
         <ul className="login-options">
           <li>
